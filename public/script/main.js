c6b8439e0fdb9bfa62e14490e1637caae8c1df79
force1267 <force1267@gmail.com>
2019-12-15 15:24:31 +0330

    ui touchup

--- public/script/main.js
+++ public/script/main.js
@@ -1196,6 +1196,9 @@ $('#adv .search').click(function() {
                     </div>
                 `)
             });
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(".cross-line").offset().top - 100
+            }, 600);
         } else {
             alert('نتیجه ای یافت نشد !');
         }
