708eec0b94c26091c7acd73c7c12aa69d9bbde25
Javad Asadi <force1267@gmail.com>
2020-01-10 01:49:14 +0330

    like block bug

--- api/post/controllers/Post.js
+++ api/post/controllers/Post.js
@@ -4,20 +4,20 @@
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-let likeAvailable = true // todo array
+let likeBlocked = [] // todo array
 
 module.exports = {
     async like(ctx) { // GET /posts/like/:id
-        if(likeAvailable) {
-            likeAvailable = false
-            let id = ctx.params.id
+        let id = ctx.params.id
+        if(!likeBlocked[id]) {
+            likeBlocked[id] = true
             try {
                 let post = await strapi.services.post.findOne({ id })
                 let result = await strapi.services.post.update({ id }, { like: post.like + 1})
-                setTimeout(e => likeAvailable = true ,3000)
+                setTimeout(e => likeBlocked[id] = false, 3000)
                 return result
             } catch (err) {
-                likeAvailable = true
+                likeBlocked[id] = false
             }
         }
     }
