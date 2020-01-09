2729a3c4139ef9b8a8d6fd9789b895066e395521
Javad Asadi <force1267@gmail.com>
2020-01-10 00:29:03 +0330

    added like

--- api/post/controllers/Post.js
+++ api/post/controllers/Post.js
@@ -4,5 +4,21 @@
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+let likeAvailable = true // todo array
 
-module.exports = {};
+module.exports = {
+    async like(ctx) { // GET /posts/like/:id
+        if(likeAvailable) {
+            likeAvailable = false
+            let id = ctx.params.id
+            try {
+                let post = await strapi.services.post.findOne({ id })
+                let result = await strapi.services.post.update({ id }, { like: post.like + 1})
+                setTimeout(e => likeAvailable = true ,3000)
+                return result
+            } catch (err) {
+                likeAvailable = true
+            }
+        }
+    }
+};
