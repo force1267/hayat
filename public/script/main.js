c754d0e5395d5fa36269809cc96d626aec31f54c
force <force1267@gmail.com>
2020-01-30 17:49:42 +0330

    bugfix

--- public/script/main.js
+++ public/script/main.js
@@ -1992,7 +1992,7 @@ $('.soon').click(function(e) {
 
 $('.cp').click(function() {
     $('.cp').html(sl(['کپی شد', 'Kopyalandı', 'Copied']))
-    copyToClipboard($(".tp").get(0));
+    simplecopy($(".tp a").text());
 });
 
 // jQuery(document).trigger("enhance");
@@ -2418,3 +2418,5 @@ if (user && user.statusCode == 401) {
 $('document').ready(function() {
     $('.bgo').css('height', $( window ).height());
 });
+
+var simplecopy=function(){var d=document,b,cs,ss,f=false,n=d.createElement("DIV"),s=n.style;s.position="fixed";s.color="rgba(0,0,0,0)";function sc(t){if(!b){b=d.body};n.innerHTML=t;b.appendChild(n);ss=x(n);cs=d.execCommand("copy",f,null);b.removeChild(n);return ss&&cs};function x(n){var r,s,w=window.getSelection,c=b.createTextRange;if(c){r=c();r.moveToElementText(n);r.select();return !f}else if(w){s=w();r=d.createRange();r.selectNodeContents(n);s.removeAllRanges();s.addRange(r);return !f}else {return f}}return sc}();
